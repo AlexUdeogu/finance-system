5eb111c10f09517f4a801e77aab6e5c3e860831a
Udeogu Alex <alexanderudeogu@gmail.com>
2024-08-20 16:39:23 +0100

    Day 2 of building

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -12,6 +12,8 @@ import Monday from '../assets/monday.png';
 import Dropbox from '../assets/dropbox.png';
 import Stripe from '../assets/stripe.png';
 import Zoom from '../assets/zoom.png';
+import Actions from '../assets/actions.png';	
+import Footer from '../assets/footer.png';
 
 
 
@@ -49,7 +51,7 @@ const Navbar = () => {
       {/* Hero Image Section */}
         <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between p-4">
             <div className="flex justify-start mb-4  lg:mb-0">
-                <img src={hero} alt="Hero" className="max-w-2xl  h-auto" />
+                <img src={hero} alt="Hero" className=" w-auto  h-auto" />
             </div>
             <div className="flex-1 flex flex-col items-center lg:items-start lg:justify-start">
                 <h1 className="text-3xl lg:text-8xl font-bold text-gray-800 text-center lg:text-left mb-5">
@@ -131,36 +133,99 @@ const Navbar = () => {
       {/* Cards Section */}
       <div className="grid grid-cols-4 gap-6">
         {/* Row 1 */}
-        <div className="bg-gray-200 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
+        <div className="bg-gray-100 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
         <img src={Airtable} alt="group" className="w-30 h-30" />
         </div>
-        <div className="bg-gray-200 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
+        <div className="bg-gray-100 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
         <img src={Discord} alt="group" className="w-30 h-30" />
         </div>
-        <div className="bg-gray-200 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
+        <div className="bg-gray-100 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
         <img src={Slack} alt="group" className="w-30 h-30" />
         </div>
-        <div className="bg-gray-200 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
+        <div className="bg-gray-100 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
         <img src={Pinterest} alt="group" className="w-30 h-30" />
         </div>
 
         {/* Row 2 */}
-        <div className="bg-gray-200 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
+        <div className="bg-gray-100 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
         <img src={Monday} alt="group" className="w-30 h-30" />
         </div>
-        <div className="bg-gray-200 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
+        <div className="bg-gray-100 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
         <img src={Dropbox} alt="group" className="w-30 h-30" />
         </div>
-        <div className="bg-gray-200 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
+        <div className="bg-gray-100 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
         <img src={Stripe} alt="group" className="w-30 h-30" />
         </div>
-        <div className="bg-gray-200 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
+        <div className="bg-gray-100 rounded-lg p-6 flex items-center justify-center min-h-[100px]">
         <img src={Zoom} alt="group" className="w-30 h-30" />
         </div>
       </div>
 
 
     </div>
+
+    {/* Get app Section */}
+    <div className="pt-20  flex justify-center items-center min-h-[400px]">
+      <div className="bg-gray-100 rounded-2xl pt-6 pb-0 p-x-0 max-w-[1000px] min-h-[300px] flex flex-col items-center justify-center">
+        <h2 className="text-4xl font-semibold text-gray-800 text-center">Get the Finzo Mobile app.</h2>
+        <p className="text-gray-600 text-lg mt-2 text-center">With this platform,you can access your account anywhere, <br/>anytime for balance and so much more.</p>
+        <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between'>
+          <a 
+              href="#login"  
+              className="text-black bg-yellow-400 hover:bg-black font-bold hover:text-white py-2 px-4 rounded-full transition duration-300 hidden lg:block mr-1"
+          >
+              Open Account
+          </a>
+
+          <a 
+              href="#login"  
+              className="text-black outline outline-offset-2 outline-1 hover:outline-none hover:bg-yellow-400 font-bold hover:text-black py-2 px-4 rounded-full transition duration-300 hidden lg:block m-4"
+          >
+              Generate Card
+          </a>
+        </div>
+        <img src={Actions} alt="group" className="w-30 h-30" />
+        
+      </div>
+    </div>
+
+
+
+    {/* footer Section */}
+  <div className="flex flex-col items-center pt-20">
+    <h1 className="text-6xl font-bold text-gray-800 pb-10">
+      Save smart. Achieve more.
+    </h1>
+
+    <div className="bg-gray-950 rounded-t-2xl p-6 pb-20 max-w-6xl w-full flex flex-col lg:flex-row">
+      {/* First Half */}
+      <div className="flex flex-col justify-center w-full lg:w-1/2">
+        <img src={Footer} alt="group" className="w-16 h-16" />
+        <h1 className="text-6xl font-bold mt-10 text-gray-800 text-center lg:text-left">
+          Keep up with the <br/> latest
+        </h1>
+        <p className="text-gray-600 text-lg mt-2 text-center lg:text-left">
+          With this platform, you can access your account anywhere, <br/> anytime for balance and so much more.
+        </p>
+      </div>
+
+      {/* Second Half */}
+      <div className="flex flex-col justify-end w-full lg:w-1/2 mt-xl lg:mt-0 lg:pl-6">
+        <p className="text-gray-800 font-bold text-2xl mb-4">
+          Stay up to date
+        </p>
+        <input
+          type="text"
+          placeholder="Enter your email"
+           className="w-full p-3 rounded-lg border-2 border-gray-300 bg-gray-100 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+</div>
+
+  </div>
+
+
+
 
 
 
