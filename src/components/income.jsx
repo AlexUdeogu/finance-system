26a0b86436465fcdcd07132e0d95dbeee4ae9394
Udeogu Alex <alexanderudeogu@gmail.com>
2024-10-08 18:20:59 +0100

    Greetings function

--- src/components/income.jsx
+++ src/components/income.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+/*import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'; */
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
 import {
